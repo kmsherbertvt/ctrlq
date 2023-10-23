f1090420133a8ebfdde6f37f81c47ffe22bda851
Kyle Sherbert <kyle.sherbert@vt.edu>
2023-10-23 19:57:09 -0400

    Make example runnable

--- example/adapt.py
+++ example/adapt.py
@@ -11,6 +11,6 @@ mypulse = cvqe.pulse(shape='square',duration=10.,nwindow=1, nqubit = 2,
                      amp_bound=0.04,freq_bound=1.5)
 
 myham = cvqe.transmon(mham = cHam,nqubit=2)
-ctrl = cvqe.control(mypulse,myham,nstep=N,solver='trotter')
+ctrl = cvqe.control(mypulse,myham,nstep=100,solver='trotter')
 
 ctrl.adapt(exactE = eval1[0])
